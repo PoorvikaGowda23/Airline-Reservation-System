7931b3cfa59c16b07ee2a50b751c9f51e2463394
PoorvikaGowda23 <147130409+PoorvikaGowda23@users.noreply.github.com>
2025-04-02 19:06:48 +0530

    final commit

---
+++ JAVA_OOPS/CaseStudy/Alertable.java
@@ -0,0 +1,7 @@
+package CaseStudy;
+
+public interface Alertable {
+    void sendAlert();
+    void logAlert();
+
+}
---
+++ JAVA_OOPS/CaseStudy/TheftReport.java
@@ -0,0 +1,14 @@
+package CaseStudy;
+import java.util.Date;
+
+public class TheftReport extends CrimeReport {
+    public TheftReport(String location, String description, Date reportDate) {
+        super(location, description, reportDate);
+    }
+    
+    @Override
+    public void generateReport() {
+        System.out.println("[Theft Report] Location: " + location + " | Severity: " + severity);
+        displayDetails();
+    }
+}
